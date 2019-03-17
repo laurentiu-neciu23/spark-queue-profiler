c07f0df710aa6e6076e1ce451a7cc5b96a109f15
Neciu Laurentiu Florin <laurentiu.neciu@gmail.com>
2019-03-17 20:10:54 +0200

    Better script for pushing workflows to lite cluster

--- docker-compose/run_workflows.sh
+++ docker-compose/run_workflows.sh
@@ -5,22 +5,29 @@ shared_folder="spark-shared"
 generation_workflows="workflows/kmeans-execute.conf"
 perf_workflows="workflows/kmeans-generate.conf"
 
-function create_folders {
-  dir_num=$(echo $1 | tr '/' ' ' | wc -w)
-  directory_path=$(echo $1 | cut -d '/' -f 1-$[$dir_num-1])
-  file_path=$(echo $1 | cut -d '/' -f $dir_num)
-  echo $directory_path
-  echo $file_path
+function split {
+  local dir_num=$(echo $1 | tr '/' ' ' | wc -w)
+  local directory_path=$(echo $1 | cut -d '/' -f 1-$[$dir_num-1])
+  local file_path=$(echo $1 | cut -d '/' -f $dir_num)
+  echo  $directory_path
+}
 
+function create_workflow {
+  local directory_path=$(split $1)
   mkdir -p "./$shared_folder/$directory_path"
   cp "./$1" "./$shared_folder/$directory_path"
 }
 
-create_folders $generation_workflows
-create_folders $perf_workflows
+function remove_workflow {
+  local directory_path$(split $1)
+  rm "./$shared_folder/$directory_path/$1"
+}
 
-# todo if many workflows 
-echo "/$shared_folder/$generation_workflows"
+create_workflow $generation_workflows
+create_workflow $perf_workflows
+
 docker exec -t spark spark-bench.sh "/$shared_folder/$generation_workflows"
 docker exec -t spark spark-bench.sh "/$shared_folder/$perf_workflows"
 
+remove_workflow $generation_workflows
+remove_workflow $perf_workflows
