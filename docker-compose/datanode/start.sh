6449bebc80aac4b7ed77ca1b0a07ba05cb173826
Neciu Laurentiu Florin <laurentiu.neciu@gmail.com>
2019-05-07 21:19:55 +0300

    Added plots for disk writes and reads

--- docker-compose/datanode/start.sh
+++ docker-compose/datanode/start.sh
@@ -2,5 +2,8 @@
 echo $HADOOP_PREFIX
 echo $HADOOP_CONF_DIR
 
+# Start node exporter
+cd node_exporter-*.*-amd64 && ./node_exporter > node_exporter.log &
+
 # Start namenode
 $HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR datanode
--- statistics_dataset_extractor.py
+++ statistics_dataset_extractor.py
@@ -55,12 +55,16 @@ def plot(pooled_stats):
 
     titles = {
         'allocated_bytes': "Execution timeline vs Allocation Bytes.", 
-        'cpu': "Execution timeline vs Cpu Percent Usage."
+        'cpu': "Execution timeline vs Cpu Percent Usage.",
+        'disk_reads': "Execution timeline vs Disk Reads Completed.",
+        'disk_writes': "Execution timeline vs Disk Writes Completed."
     }
 
     ylabels = {
         'allocated_bytes': "Allocated memory (GB).", 
-        'cpu': "Mean CPU utilization since process started (%)."
+        'cpu': "Mean CPU utilization since process started (%).",
+        'disk_reads': "Total disk reads since process started (4KB page).",
+        'disk_writes': "Total disk wr itessince process started (4KB page)."
     }
 
     patches = [mpatches.Patch(color=color, label=label) for label, color in patches_colors.items()]
@@ -93,8 +97,10 @@ def await_async(process):
 def pool():
     target_time = 200
     running_time = 0
-    data = {'running_time': [], 'allocated_bytes': [], 'cpu': []}
+    data = {'running_time': [], 'allocated_bytes': [], 'cpu': [], 'disk_reads': [], 'disk_writes': []}
     starting_user_times, starting_total_times = fetch_cpu()
+    start_disk_writes = fetch_disk_writes_completed()
+    start_disk_reads = fetch_disk_reads_completed()
 
     while running_time < target_time:
         time_start = time.time()
@@ -102,28 +108,34 @@ def pool():
         allocated_bytes = fetch_allocated_bytes()
         user_times, total_times = fetch_cpu()
         cpu = ((user_times - starting_user_times) / (total_times - starting_total_times)) * 100
+        disk_writes = fetch_disk_writes_completed() - start_disk_writes
+        disk_reads = fetch_disk_reads_completed() - start_disk_reads
 
         time.sleep(1.5)
         time_finish = time.time()
         running_time += time_finish - time_start
         print("[INFO][StatisticsDatasetExtractor][MEM] Detected allocated_bytes of: " + str(allocated_bytes) + " at time: " + str(running_time) + ".")
         print("[INFO][StatisticsDatasetExtractor][CPU] Detected cpu of: " + str(cpu) + "at time: " + str(running_time) + ".")
-        
+        print("[INFO][StatisticsDatasetExtractor][DISK_WRITES] Detected disk_writes of: " + str(disk_writes) + "at time: " + str(running_time) + ".")
+        print("[INFO][StatisticsDatasetExtractor][DISK_READS] Detected disk_reads of: " + str(disk_reads) + "at time: " + str(running_time) + ".")
+
+
         data['running_time'].append(running_time)
         data['allocated_bytes'].append(allocated_bytes)
+        data['disk_reads'].append(disk_reads)
+        data['disk_writes'].append(disk_writes)
         data['cpu'].append(cpu)
 
     return pd.DataFrame(data=data)
 
 
-def fetch_all():
-    port = 9100
+def fetch_all(port):
     response = requests.get('http://localhost:' + str(port) + '/metrics')
     return response.text.split('\n')           
         
 
 def fetch_allocated_bytes():
-    parsed_lines = fetch_all()           
+    parsed_lines = fetch_all(9100)           
     free_bytes = list(filter(lambda x: x.startswith('node_memory_MemAvailable_bytes '), parsed_lines))
     free_bytes_float = float(free_bytes[0].split(' ')[1])
     
@@ -134,7 +146,7 @@ def fetch_allocated_bytes():
 
 
 def fetch_cpu():
-    parsed_lines = fetch_all()
+    parsed_lines = fetch_all(9100)
 
     # node_cpu_seconds_total{cpu="0",mode="idle"} 1837.25
     # node_cpu_seconds_total{cpu="0",mode="iowait"} 6.5
@@ -155,6 +167,20 @@ def fetch_cpu():
     total_times = functools.reduce(lambda x, y: x + float(y[4]), time_cpu_lines, 0)
     user_times = functools.reduce(lambda x, y: x + float(y[4]), user_time_cpu_lines, 0)
     return (user_times, total_times)
+
+def fetch_disk_writes_completed():
+    parsed_lines = fetch_all(9103)
+    disk_writes_completed_line = list(filter(lambda x: x.startswith('node_disk_writes_completed_total{device="sda"} '), parsed_lines))
+    disk_writes = float(disk_writes_completed_line[0].split(' ')[1])
+    return disk_writes
+
+
+def fetch_disk_reads_completed():
+    parsed_lines = fetch_all(9103)   
+    disk_reads_completed_line = list(filter(lambda x: x.startswith('node_disk_reads_completed_total{device="sda"} '), parsed_lines))
+    disk_reads = float(disk_reads_completed_line[0].split(' ')[1])
+    return disk_reads 
+
 
 
 
